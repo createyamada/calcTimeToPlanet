18dcefe8de906ce473e2c7f14670a735770ed80e
createyamada <yamadayeah90@gmail.com>
2023-06-07 17:46:41 +0900

    計算処理実装

--- lib/result_page.dart
+++ lib/result_page.dart
@@ -2,8 +2,8 @@
 import 'package:flutter/material.dart';
 
 class ResultPage extends StatelessWidget {
-  ResultPage(this.name);
-  final String name;
+  ResultPage(this.result);
+  final Map result;
   
   @override
   Widget build(BuildContext context) {
@@ -14,7 +14,14 @@ class ResultPage extends StatelessWidget {
       body: Center(
         child: Column(
           children: [
-            Text(name),
+            Text('太陽まで' + result["sun"]),
+            Text('水星まで' + result['mercury']),
+            Text('金星まで' + result['venus']),
+            Text('火星まで' + result['mars']),
+            Text('木星まで' + result['jupiter']),
+            Text('土星まで' + result['saturn']),
+            Text('海王星まで' + result['uranus']),
+            Text('天の川銀河の中心ブラックホールまで' + result['itezaAstar']),
           ],
         ) ,
       ),
