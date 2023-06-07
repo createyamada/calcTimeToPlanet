3f0894242806c3d011ecb7f758dcc62c718c6e3b
createyamada <yamadayeah90@gmail.com>
2023-06-07 23:14:00 +0900

    光の比較計算追加

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,8 @@ import 'package:calc_time_to_planet/result_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
+import 'const.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -26,116 +28,80 @@ class calcPage extends StatefulWidget {
 
 class _calcPage extends State<calcPage> {
   double valueText = 0;
-  Map<String,dynamic> result = {};
+  Map<String, dynamic> result = {};
 
   /**
 	* 入力値を時速に変換して計算
   * @param null
   * @return Map result
 	*/
- void calcTime() {
-  print(valueText);
-  // 速さを時速KMに変換
-  double speed = (((50 / valueText) * 60) * 60) / 1000;
+  void calcTime() {
+    print(valueText);
+    // 速さを時速KMに変換
+    double speed = (((50 / valueText) * 60) * 60) / 1000;
 
-  print('速度計算結果' + speed.toString());
+    print('速度計算結果' + speed.toString());
 
-  //　太陽
-  const sun = 150000000;
-  
-  result['sun'] = convDate(sun / speed);
+    //　太陽
+    result['sun'] = convDate(sun / speed);
 
-  //　水星
-  const mercury = 230000000;
-  result['mercury'] = convDate(mercury / speed);
+    //　水星
+    result['mercury'] = convDate(mercury / speed);
 
-  //　金星
-  const venus = 150000000;
-  result['venus'] = convDate(venus / speed);
+    //　金星
+    result['venus'] = convDate(venus / speed);
 
-  //　火星
-  const mars = 150000000;
-  result['mars'] = convDate(mars / speed);
+    //　火星
+    result['mars'] = convDate(mars / speed);
 
-  //　木星
-  const jupiter = 750000000;
-  result['jupiter'] = convDate(jupiter / speed);
+    //　木星
+    result['jupiter'] = convDate(jupiter / speed);
 
-  //　土星
-  const saturn = 1500000000;
-  result['saturn'] = convDate(saturn / speed);
+    //　土星
+    result['saturn'] = convDate(saturn / speed);
 
-  //　天王星
-  const uranus = 2900000000;
-  result['uranus'] = convDate(uranus / speed);
+    //　天王星
+    result['uranus'] = convDate(uranus / speed);
 
-  // 天の川銀河中心のブラックホールいて座A＊
-  const itezaAstar = 9500000000000 * 25640;
-  result['itezaAstar'] = convDate(itezaAstar / speed);
+    // 天の川銀河中心のブラックホールいて座A＊
+    result['itezaAstar'] = convDate(itezaAstar / speed);
 
+    print("結果配列の表示" + result.toString());
 
-  print("結果配列の表示" + result.toString());
-  
-  setState(() {});
- }
-
-  /**
-	* 時間を日数に変換する
-  * @param double time
-  * @return 
-	*/
-  String convDate(double time) {
-    // 年数を計算する
-    double year = time / 8760;
-    print("convDate:年数" + year.toString());
-
-    // 月数を計算する
-    double month = (time % 8760) / 730;
-    print("convDate:月数" + month.toString());
-
-    // 日数を計算する
-    double day = ((time % 8760 ) % 730) / 24;
-    print("convDate:日数" + day.toString());
-
-    return year.floor().toString() + "年" + month.floor().toString() + "カ月" + day.floor().toString() + "日";
+    setState(() {});
   }
 
-  @override 
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('惑星までの速度計算'),
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [ 
-              TextField(
-                keyboardType: TextInputType.number,
-                inputFormatters: [
-                  // FilteringTextInputFormatter.digitsOnly
-                ],
-                onChanged: (value) {
-                  valueText = double.parse(value);
-                },
-                decoration: InputDecoration(
-                  hintText: 'あなたの50Mのタイムを入力してください。'
-                ),
-              ),
-              ElevatedButton(
-                onPressed: () {
-                  calcTime();
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => ResultPage(result),)
-                  );
-                }, 
-                child: Text('計算開始'),
-              )
-            ]
+        child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+          TextField(
+            keyboardType: TextInputType.number,
+            inputFormatters: [
+              // FilteringTextInputFormatter.digitsOnly
+            ],
+            onChanged: (value) {
+              valueText = double.parse(value);
+            },
+            decoration: InputDecoration(hintText: 'あなたの50Mのタイムを入力してください。'),
           ),
-        ),
-      );
+          ElevatedButton(
+            onPressed: () {
+              calcTime();
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => ResultPage(result),
+                  ));
+            },
+            child: Text('計算開始'),
+          )
+        ]),
+      ),
+    );
   }
 }
-
--- lib/result_page.dart
+++ lib/result_page.dart
@@ -1,10 +1,11 @@
-
 import 'package:flutter/material.dart';
+
+import 'const.dart';
 
 class ResultPage extends StatelessWidget {
   ResultPage(this.result);
   final Map result;
-  
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -15,15 +16,23 @@ class ResultPage extends StatelessWidget {
         child: Column(
           children: [
             Text('太陽まで' + result["sun"]),
+            Text('光の速さだと' + convDateByLight(light_result["sun"])),
             Text('水星まで' + result['mercury']),
+            Text('光の速さだと' + convDateByLight(light_result["mercury"])),
             Text('金星まで' + result['venus']),
+            Text('光の速さだと' + convDateByLight(light_result["venus"])),
             Text('火星まで' + result['mars']),
+            Text('光の速さだと' + convDateByLight(light_result["mars"])),
             Text('木星まで' + result['jupiter']),
+            Text('光の速さだと' + convDateByLight(light_result["jupiter"])),
             Text('土星まで' + result['saturn']),
+            Text('光の速さだと' + convDateByLight(light_result["saturn"])),
             Text('海王星まで' + result['uranus']),
+            Text('光の速さだと' + convDateByLight(light_result["uranus"])),
             Text('天の川銀河の中心ブラックホールまで' + result['itezaAstar']),
+            Text('光の速さだと25640年'),
           ],
-        ) ,
+        ),
       ),
     );
   }
